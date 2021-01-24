8f661520df497ff5225d34ce9e7f7daa2ba55013
igortrivinho <igortmf17@gmail.com>
2021-01-24 18:15:37 -0300

    Add project files.

---
+++ Organizador_Aniversarios/Aniversariante.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Organizador_Aniversarios
+{
+    class Aniversariante
+    {
+        // ========== Atributos Privados e Auto Properties =============
+        private string _nome;
+        public DateTime Aniversario { get; private set; }
+
+        // ======================= Construtores ========================
+        public Aniversariante(string nome, DateTime aniversario)
+        {
+            _nome = nome;
+            Aniversario = aniversario;
+        }
+
+        // ================== Properties Customizadas ==================
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        // ========================== Métodos ==========================
+        public string MudarAniversario (DateTime novaData)
+        {
+            Aniversario = novaData;
+            return "Aniversário alterado com sucesso!";
+        }
+
+        // ======================== To String ==========================
+        public override string ToString()
+        {
+            return Aniversario.ToString("dd-MM") + " -> " + Nome;
+        }
+    }
+}
---
+++ Organizador_Aniversarios/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Organizador_Aniversarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = null;
+
+            Console.Write("Nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Data de Aniversário: (dia/mês/ano) ");
+            string dataUsuario = Console.ReadLine();
+
+            DateTime data = DateTime.ParseExact(dataUsuario, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+            Aniversariante pessoa = new Aniversariante(nome, data);
+
+            Console.WriteLine("\n" + pessoa);
+        }
+    }
+}
